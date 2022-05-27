c4ee8fa18a6800a1c5e539f63d662deefb5f59fa
zhangbo <zhangbo_dcits@163.com>
2022-05-27 15:00:07 +0800

    2

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import pytest
 
 if __name__ == '__main__':
     pytest.main()
-    os.system("allure generate ./report --clean allure-reports -o ./report/html")
-    print(111111)
+    # os.system("allure generate ./report --clean allure-reports -o ./report/html")
+    # print(111111)
